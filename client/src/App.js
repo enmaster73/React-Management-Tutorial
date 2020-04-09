102e9d81aa5ef3bdd2e65c9331219a93d9463299
junghopark <jungho.park@cdnetworks.co.kr>
2020-04-09 16:42:57 +0900

    add get customer rest api

--- client/src/App.js
+++ client/src/App.js
@@ -22,33 +22,24 @@ const styles = theme => ({
   }
 })
 
-const customers = [{
-  'id':1,
-  'image': 'https://placeimg.com/64/64/any',
-  'name':'gildong hong',
-  'birthday':'7307231',
-  'gender': 'male',
-  'job':'student'
-},
-{
-  'id':2,
-  'image': 'https://placeimg.com/64/64/any',
-  'name':'jungho park',
-  'birthday':'7007231',
-  'gender': 'male',
-  'job':'student'
-}
-,
-{
-  'id':3,
-  'image': 'https://placeimg.com/64/64/any',
-  'name':'siyun park',
-  'birthday':'7207231',
-  'gender': 'male',
-  'job':'student'
-}]
-
 class App extends Component {
+
+  state = {
+    customers:""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render() {
     const {classes } = this.props;
     return (
@@ -66,7 +57,9 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map(c=> {return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)})}
+            {this.state.customers ? this.state.customers.map(c=> {
+              return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />)
+            }) : ""}
           </TableBody>
         </Table>
 
--- server.js
+++ server.js
@@ -6,8 +6,34 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message:'Hello Express!'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+            'id':1,
+            'image': 'https://placeimg.com/64/64/any',
+            'name':'gildong hong',
+            'birthday':'7307231',
+            'gender': 'male',
+            'job':'student'
+          },
+          {
+            'id':2,
+            'image': 'https://placeimg.com/64/64/any',
+            'name':'jungho park',
+            'birthday':'7007231',
+            'gender': 'male',
+            'job':'student'
+          }
+          ,
+          {
+            'id':3,
+            'image': 'https://placeimg.com/64/64/any',
+            'name':'siyun park',
+            'birthday':'7207231',
+            'gender': 'male',
+            'job':'student'
+          } 
+    ]);
 })
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
