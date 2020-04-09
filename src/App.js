309f7b02bca44cc617d11e5544458a55d9258de2
junghopark <jungho.park@cdnetworks.co.kr>
2020-04-09 12:09:48 +0900

    create customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/customer';
 import './App.css';
+
+
+const customers = [{
+  'id':1,
+  'image': 'https://placeimg.com/64/64/any',
+  'name':'gildong hong',
+  'birthday':'7307231',
+  'gender': 'male',
+  'job':'student'
+},
+{
+  'id':2,
+  'image': 'https://placeimg.com/64/64/any',
+  'name':'jungho park',
+  'birthday':'7007231',
+  'gender': 'male',
+  'job':'student'
+}
+,
+{
+  'id':3,
+  'image': 'https://placeimg.com/64/64/any',
+  'name':'siyun park',
+  'birthday':'7207231',
+  'gender': 'male',
+  'job':'student'
+}]
 
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-        <h2>Let's develop management system!</h2>
-      </div>
+      <div>
+        {
+          customers.map(c=> {
+            return (
+              <Customer 
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+                />
+            )
+
+          })
+        }
+
+        </div>
     );
   }
 }
